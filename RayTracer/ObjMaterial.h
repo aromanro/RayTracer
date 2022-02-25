8e3f58125fc0f3e032a88863098a830306be6d46
Adrian Roman <a.roman@ib-company.de>
2022-02-25 15:32:23 +0200

    Probably better to set specular color to diffuse color instead of 1.

--- RayTracer/ObjMaterial.h
+++ RayTracer/ObjMaterial.h
@@ -81,7 +81,7 @@ public:
 
 		if (!specularTexture.empty() && specularColor.TotalAbsorbant())
 		{
-			specularColor.r = specularColor.g = specularColor.b = 1;
+			specularColor = diffuseColor;
 		}
 	}
 
